6ff4dfc57a2473b58d0070c204cf821c3c2f51b8
Caesar <ignatiuscaesar16@gmail.com>
2024-03-21 20:49:44 +0300

    Edits the js file

---
+++ index.js
@@ -0,0 +1,21 @@
+// Function Expression 'divide'
+// This arrow function, named 'divide', performs a division operation. 
+// It's defined without parameters and returns the result of 2000 divided by 100. 
+// The absence of parameters is indicated by empty parentheses.
+const divide = () => 2000 / 100;
+
+// Arrow Function 'square'
+// The 'square' function takes a single parameter 'x' and returns its square (x multiplied by itself).
+// As this function has only one parameter, parentheses are optional but used here for clarity. 
+// It demonstrates an efficient use of arrow functions for simple operations.
+const square = (x) => x * x;
+
+// Arrow Function 'add'
+// This function, 'add', demonstrates the use of arrow functions with multiple parameters.
+// It takes two parameters, 'a' and 'b', and returns their sum. This showcases how arrow functions
+// can be used for straightforward operations requiring more than one input.
+const add = (a, b) => a + b;
+
+// By employing arrow functions, we achieve a concise and readable syntax for defining functions.
+//  Arrow functions are especially useful for inline functions and callbacks,
+// contributing to cleaner and more maintainable code.
